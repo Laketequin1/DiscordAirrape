38f7cc363f3153b550bf849ea56e2bc40801c8dc
Laketequin1 <Laketequin1@gmail.com>
2023-01-05 21:38:09 +1300

    Audio works!!!

---
+++ build.bat
@@ -0,0 +1,2 @@
+g++ -c main.cpp -o main.o -IC:\SFML-2.5.1\include
+g++ -L"C:\SFML-2.5.1\lib" .\test.o -o test.exe -lsfml-audio
--- main.cpp
+++ main.cpp
@@ -1,52 +1,24 @@
-#include <windows.h>
+#include <SFML/Audio.hpp>
 #include <iostream>
-
-// Function that takes a decimal value between 0 and 1 and returns the corresponding
-// volume level in the format required by the waveOutSetVolume function.
-DWORD GetVolumeLevelFromDecimal(float volume)
-{
-    // Clamp the volume value to the range [0, 1].
-    volume = std::max(0.0f, std::min(1.0f, volume));
-
-    // Convert the volume value to the range [0x0000, 0xFFFF].
-    return static_cast<DWORD>(volume * 0xFFFF);
-}
 
 int main()
 {
-    // Specify whether the sound should loop or only play once.
-    bool loopSound = true;
+    std::cout << "Start";
 
-    // Set the flags for the PlaySound function.
-    DWORD flags = SND_ASYNC | SND_FILENAME;
-    if (loopSound)
+    // Load the sound buffer from a file
+    sf::SoundBuffer buffer;
+    if (!buffer.loadFromFile("test.wav"))
+        return -1;
+
+    // Create a sound instance and play it
+    sf::Sound sound;
+    sound.setBuffer(buffer);
+    sound.play();
+
+    // Keep the program running until the sound finishes playing
+    while (sound.getStatus() == sf::Sound::Playing)
     {
-        flags |= SND_LOOP;
-    }
-
-    // Play the wav file
-    if (!PlaySound(TEXT("star.wav"), NULL, flags))
-    {
-        std::cerr << "Failed to play sound." << std::endl;
-        return 1;
-    }
-
-    std::cout << "Playing sound..." << std::endl;
-
-    // Gradually increase the volume an amount per time period.
-    float volume = 0.0f;
-    const float volumeIncrease = 0.2f;
-    const float increaseDuration = 1.0f;
-    while (true)
-    {
-        // Calculate the volume level as a function of time.
-        volume += volumeIncrease;
-
-        // Set the volume.
-        waveOutSetVolume(NULL, GetVolumeLevelFromDecimal(volume));
-
-        // Sleep for the specified time step.
-        Sleep(static_cast<DWORD>(increaseDuration * 1000.0f));
+        // Do nothing
     }
 
     return 0;
